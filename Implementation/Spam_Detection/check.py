d57024d38c242fec779be62efed7f422976b7516
anchal786 <71549950+anchal786@users.noreply.github.com>
2025-04-06 21:07:46 -0700

    Update check.py

--- Implementation/Spam_Detection/check.py
+++ Implementation/Spam_Detection/check.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 df = pd.read_csv("../data/enron_emails.csv")  # Adjust path if needed
 print(df.columns)
-print(df.head())  # Show first 5 rows
+print(df.head())  # Show first 5 rows 
